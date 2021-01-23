e7907fab4bd9b06773100e32dd9914a048a5c6b7
michael belete <it.michael.belete@gmail.com>
2021-01-23 08:21:11 -0500

    used prompt to receive data and displayed it on console

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-alert("hello, world")
+var firstName, lastName, age,  job;
+
+firstName = prompt("enter your first name")
+lastName = prompt("enter your last name")
+age = prompt("enter your age")
+job = prompt("enter your profession")
+
+console.log("Here is your Profile ")
+console.log("Full Name: " + firstName + " "+lastName);
+console.log("Profession : " + job);
+console.log("Age : " + age + " " + "years old");
