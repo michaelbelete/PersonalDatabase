372858fe10de7a11c1f821526fd3ffd67fbf84a8
michael belete <it.michael.belete@gmail.com>
2021-01-23 08:27:40 -0500

    check age eligibility to vote using age

--- assets/js/app.js
+++ assets/js/app.js
@@ -5,7 +5,18 @@ lastName = prompt("enter your last name")
 age = prompt("enter your age")
 job = prompt("enter your profession")
 
+// check if the user can vote
+
+let tempAge;
+let isEligiableToVote;
+
+tempAge = parseInt(age)
+
+isEligiableToVote = tempAge >= 18 ? true : false;
+
+// display result
 console.log("Here is your Profile ")
 console.log("Full Name: " + firstName + " "+lastName);
 console.log("Profession : " + job);
 console.log("Age : " + age + " " + "years old");
+console.log("Eligable to vote : " + isEligiableToVote)
