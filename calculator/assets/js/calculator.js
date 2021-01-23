dc989969216686a174b735879638a4bc455e053b
michael belete <it.michael.belete@gmail.com>
2021-01-23 15:58:00 -0500

    added while loop to not stop calculating

--- calculator/assets/js/calculator.js
+++ calculator/assets/js/calculator.js
@@ -10,52 +10,55 @@ function add(numArray) {
 function mult(numArray) {
     let result = 1
     numArray.forEach(num => {
-        result = result * num 
+        result = result * num
     });
     return result
 }
 
 
-function sub(a,b) {
+function sub(a, b) {
     return a - b
-} 
+}
 
-function div(a,b) {
-    if(b == 0){
+function div(a, b) {
+    if (b == 0) {
         return 'Oops, can\'t divide by zero'
-    }else{
-        return a/b
+    } else {
+        return a / b
     }
 }
 
 (function () {
-    choice = parseInt(prompt("Choose operation: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"))
+    while (true) {
+        choice = parseInt(prompt("Choose operation: 1 for addition, 2 for subtraction, 3 for multiplication, 4 for division"))
 
-    if(choice === 1) {
-        let numArray = new Array()
-        let numberOfInputs = parseInt(prompt("please enter how much number you want to add: "))
-        
-        for(let i = 0; i < numberOfInputs; i++) {
-            numArray[i] = parseInt(prompt(`please enter ${i+1} number`))
+        if (choice === 1) {
+            let numArray = new Array()
+            let numberOfInputs = parseInt(prompt("please enter how much number you want to add: "))
+
+            for (let i = 0; i < numberOfInputs; i++) {
+                numArray[i] = parseInt(prompt(`please enter ${i + 1} number`))
+            }
+            console.log(add(numArray))
+        } else if (choice === 2) {
+            let num1 = parseInt(prompt("please enter the first number: "))
+            let num2 = parseInt(prompt("please enter the second number: "))
+
+            console.log(sub(num1, num2))
+        } else if (choice === 3) {
+            let numArray = new Array()
+            let numberOfInputs = parseInt(prompt("please enter how much number you want to multiply: "))
+
+            for (let i = 0; i < numberOfInputs; i++) {
+                numArray[i] = parseInt(prompt(`please enter ${i + 1} number`))
+            }
+            console.log(mult(numArray))
+        } else if (choice === 4) {
+            let num1 = parseInt(prompt("please enter the first number: "))
+            let num2 = parseInt(prompt("please enter the second number: "))
+
+            console.log(div(num1, num2))
         }
-        console.log(add(numArray))
-    }else if(choice === 2){
-        let num1 = parseInt(prompt("please enter the first number: "))
-        let num2 = parseInt(prompt("please enter the second number: "))
-
-        console.log(sub(num1, num2))
-    }else if(choice === 3){
-        let numArray = new Array()
-        let numberOfInputs = parseInt(prompt("please enter how much number you want to multiply: "))
-        
-        for(let i = 0; i < numberOfInputs; i++) {
-            numArray[i] = parseInt(prompt(`please enter ${i+1} number`))
-        }
-        console.log(mult(numArray))
-    }else if(choice === 4){
-        let num1 = parseInt(prompt("please enter the first number: "))
-        let num2 = parseInt(prompt("please enter the second number: "))
-
-        console.log(div(num1, num2))
     }
+
 }());
