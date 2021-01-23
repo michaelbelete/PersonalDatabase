e00e98912e51a80f7ef5bff0d8fc290cdffef3a6
michael belete <it.michael.belete@gmail.com>
2021-01-23 09:37:42 -0500

    finished family members filling prompt

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,39 @@
-var firstName, lastName, age,  job;
+//working with loops
 
-firstName = prompt("enter your first name")
-lastName = prompt("enter your last name")
-age = prompt("enter your age")
-job = prompt("enter your profession")
+let familyMembers = new Array()
 
-// check if the user can vote
+let numOfFamilyMembers = parseInt(prompt('enter number of family memebers'));
 
-let tempAge;
-let isEligiableToVote;
+for(let i = 0; i < numOfFamilyMembers; i++){
+    familyMembers[i] = prompt(`your family member number ${i+1}`)
+}
 
-tempAge = parseInt(age)
+console.log(familyMembers)
 
-isEligiableToVote = tempAge >= 18 ? true : false;
+familyMembers.forEach(function(member){
+    console.log(`your family members ${member}`)
+})
 
-// display result
-console.log("Here is your Profile ")
-console.log("Full Name: " + firstName + " "+lastName);
-console.log("Profession : " + job);
-console.log("Age : " + age + " " + "years old");
-console.log("Eligable to vote : " + isEligiableToVote)
+// // working with input and output
+// var firstName, lastName, age,  job;
+
+// firstName = prompt("enter your first name")
+// lastName = prompt("enter your last name")
+// age = prompt("enter your age")
+// job = prompt("enter your profession")
+
+// // check if the user can vote
+
+// let tempAge;
+// let isEligiableToVote;
+
+// tempAge = parseInt(age)
+
+// isEligiableToVote = tempAge >= 18 ? true : false;
+
+// // display result
+// console.log("Here is your Profile ")
+// console.log("Full Name: " + firstName + " "+lastName);
+// console.log("Profession : " + job);
+// console.log("Age : " + age + " " + "years old");
+// console.log("Eligable to vote : " + isEligiableToVote )
