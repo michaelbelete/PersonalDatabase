1c4fbc9ebd733f17eb466b99406ac256e2e54696
michael belete <it.michael.belete@gmail.com>
2021-01-24 08:06:25 -0500

    finished create account and see all accounts

--- bank/assets/js/bank.js
+++ bank/assets/js/bank.js
@@ -1,50 +1,93 @@
-(function() {
-    while(true){
-        // let choice = pARS
-    }
-}());
-
 const bankAccounts = {
     accounts: [],
-    generateAccount: function (min, max) {
-        return Math.floor(
-            Math.random() * (max - min + 1) + min
-        )
-    },
-    allAccounts: function() {
-        console.table(this.accounts)
-    },
-    addAccount: function (accountData) {
-        accountData.accountNumber = this.generateAccount(1000, 100000)
-        this.accounts.push(accountData)
-    },
-    deposit: function (money) {
-        if (money <= 0) {
-            return "please enter a valid amount"
-        } else {
-            this.accounts.balance += money
-        }
-    },
-    withdraw: function (money) {
-        if (money <= 0) {
-            return "please enter a valid amount"
-        } else {
-            if (this.accounts.balance >= 50 || this.accounts.balance < money) {
-                return "can't withdraw money"
-            } else {
-                this.accounts.balance -= money
-            }
-        }
-    },
-    balance: function (accountNumber) {
-        const selectedAccountNumber = this.accounts.filter(account => account.accountNumber === accountNumber)
-        if (selectedAccountNumber.length === 0) {
-            return "the account doesn't exist. but you can go and create account"
-        } else {
-            return selectedAccountNumber[0].balance;
-        }
-    },
-    transfer: function(sender, receiver, amount) {
-        console.log("no way jose")
+}
+
+function generateAccount(min, max) {
+    return Math.floor(
+        Math.random() * (max - min + 1) + min
+    )
+}
+function allAccounts() {
+    const accounts = bankAccounts.accounts
+    return accounts.length === 0 ? "No account has been registered" : accounts
+}
+function addAccount(accountData) {
+    if (accountData.balance < 25) {
+        return "initial deposit must be at least 25 birr"
+    } else {
+        accountData.accountNumber = generateAccount(1000, 100000)
+        bankAccounts.accounts.push(accountData)
+        return "account has been created"
     }
 }
+function deposit(accountNumber, money) {
+    const selectedAccountNumber = bankAccounts.accounts.filter(account => account.accountNumber === accountNumber)
+    if (selectedAccountNumber.length === 0) {
+        return "the account doesn't exist. but you can go and create account"
+    } else {
+        if (money <= 0 || money <= 25) {
+            return "please enter at least minimum of 25 birr"
+        } else {
+            selectedAccountNumber.accounts.balance += money
+        }
+    }
+}
+
+function withdraw(accountNumber, money) {
+    const selectedAccountNumber = bankAccounts.accounts.filter(account => account.accountNumber === accountNumber)
+    if (selectedAccountNumber.length === 0) {
+        return "the account doesn't exist. but you can go and create account"
+    } else {
+        if (money <= 0) {
+            return "please enter a valid amount"
+        } else {
+            if (bankAccounts.accounts.balance >= 50 || bankAccounts.accounts.balance < money) {
+                return "can't withdraw money"
+            } else {
+                bankAccounts.accounts.balance -= money
+            }
+        }
+    }
+}
+
+function balance(accountNumber) {
+    const selectedAccountNumber = bankAccounts.accounts.filter(account => account.accountNumber === accountNumber)
+    if (selectedAccountNumber.length === 0) {
+        return "the account doesn't exist. but you can go and create account"
+    } else {
+        return selectedAccountNumber[0].balance;
+    }
+}
+function transfer(sender, receiver, amount) {
+    console.log("no way jose")
+}
+
+(function () {
+    cont = true
+    while (cont) {
+        let choice = parseInt(prompt("Welcome to BMsys \n Choose what you want to do: \n 1) add Account \n 2) See all accounts \n 3) deposit money \n 4) withdraw money \n 5) See balance \n 6) Transfer Money \n press any other key to quit"))
+
+        if (choice === 1) {
+            let accountData = {
+                fullName: null,
+                balance: null,
+            }
+            accountData.fullName = prompt("Enter Your full Name: ")
+            accountData.balance = parseFloat(prompt("Enter initial Deposit: "))
+            const result = addAccount(accountData)
+            console.table(bankAccounts.accounts)
+            console.log(result)
+            alert(result)
+        } else if (choice === 2) {
+            console.log("**************************************")
+            console.log("********** All Accounts ****************")
+            console.table(allAccounts())
+            console.log("**************************************")
+
+        } else {
+            console.log("Exiting")
+            cont = false
+        }
+    }
+
+}());
